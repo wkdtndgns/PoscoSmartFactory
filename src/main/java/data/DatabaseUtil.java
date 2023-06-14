d75d9d0b75b85056b453762eb6cfc50db299b7fe
Minsoo Kim <holiday1883@gmail.com>
2023-06-14 21:01:50 +0900

    feat : add jdbc template

---
+++ src/main/java/data/DatabaseUtil.java
@@ -0,0 +1,31 @@
+package data;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.List;
+
+public class DatabaseUtil {
+    private JdbcTemplate jdbcTemplate;
+
+    public DatabaseUtil(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void connect() {
+        // 데이터베이스 연결 설정
+        String url = "jdbc:mysql://localhost:3306/db01";
+        String username = "root";
+        String password = "1234";
+        jdbcTemplate.setDataSource(new DriverManagerDataSource(url, username, password));
+        // 데이터베이스 연결
+
+    }
+
+    public List<String> getAllNames() {
+        String sql = "SELECT name FROM table01";
+        List<String> names = jdbcTemplate.queryForList(sql, String.class);
+        return names;
+    }
+}
