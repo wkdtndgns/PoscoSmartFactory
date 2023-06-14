179fa69b71940b4247d2d7c185fb6c38c8605bcb
shjang <wkdtndgns@naver.com>
2023-06-14 23:14:37 +0900

    temp

--- src/main/java/Util/JPAConfig.java
+++ src/main/java/Util/JPAConfig.java
@@ -2,23 +2,35 @@ package Util;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
-import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
-public class JPAConfig{
+public class JPAConfig {
   @Bean
-  public LocalEntityManagerFactoryBean getEntityManagerFactoryBean(){
-    LocalEntityManagerFactoryBean factoryBean = new LocalEntityManagerFactoryBean();
-    factoryBean.setPersistenceUnitName("PERSISTENCE");
-    return factoryBean;
+  public DataSource dataSource(){
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+    dataSource.setUrl("jdbc:mysql://localhost:3306/SmartFactory");
+    dataSource.setUsername("root");
+    dataSource.setPassword("1234");
+    return dataSource;
   }
+
   @Bean
-  public JpaTransactionManager getJpaTransactionManager(){
-    JpaTransactionManager transactionManager = new JpaTransactionManager();
-    transactionManager.setEntityManagerFactory(getEntityManagerFactoryBean().getObject());
-    return transactionManager;
+  public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+    LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+    em.setDataSource(dataSource());
+    em.setPackagesToScan(new String[] {"Domain.*"});
+    JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+    em.setJpaVendorAdapter(vendorAdapter);
+    return em;
   }
 }
